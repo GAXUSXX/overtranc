83d2849f5d7b878db1755aeaeddcb4485790960c
GAXUSXX <mchnet3@gmail.com>
2014-05-23 13:54:38 +0900

    merge fix

--- src/rui/app/transcalc/HexaActivity.java
+++ src/rui/app/transcalc/HexaActivity.java
@@ -446,7 +446,7 @@ public class HexaActivity extends Activity{
 				ClipboardManager cm = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
 				// �ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾌコ�ｿｽs�ｿｽ[
 				cm.setText(Decimal);
-				Toast.makeText(getBaseContext(), "�ｿｽR�ｿｽs�ｿｽ[�ｿｽ�ｿｽ�ｿｽﾜゑｿｽ�ｿｽ�ｿｽ", Toast.LENGTH_SHORT).show();
+				Toast.makeText(getBaseContext(), "コピーしました。", Toast.LENGTH_SHORT).show();
 				alertDialog.dismiss();
 			}
 		});
